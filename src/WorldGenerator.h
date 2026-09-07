4b47318d6d6b5c39ec08a2ae41727d20b899d8db
zinjeck <asundergameco@gmail.com>
2026-09-06 22:38:56 -0400

    Add configurable world generation settings

--- src/WorldGenerator.h
+++ src/WorldGenerator.h
@@ -3,6 +3,15 @@
 #include <SDL3/SDL.h>
 #include <cstdint>
 
+struct WorldSettings
+{
+    float averageElevation = 0.50f;
+    float temperature = 0.50f;
+    float waterLevel = 0.56f;
+    float moisture = 0.50f;
+    float continentScale = 0.50f;
+};
+
 struct GeneratedWorld
 {
     SDL_Texture* texture = nullptr;
@@ -14,6 +23,12 @@ struct GeneratedWorld
 class WorldGenerator
 {
 public:
-    static GeneratedWorld generate(SDL_Renderer* renderer, int width, int height, std::uint32_t seed);
+    static GeneratedWorld generate(
+        SDL_Renderer* renderer,
+        int width,
+        int height,
+        std::uint32_t seed,
+        const WorldSettings& settings);
+
     static void destroy(GeneratedWorld& world);
 };
